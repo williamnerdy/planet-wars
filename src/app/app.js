9e806720bcbb1795a707b4b44a149162c2b37cb8
William Cunha <william.cunha@wololo.tech>
2019-05-01 12:21:30 -0300

    Add content to App

--- src/app/app.js
+++ src/app/app.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Background from '../components/background';
+import Game from '../components/game';
+import LoadingGame from '../components/loading-game';
+import { Planets } from '../swapi';
+import { shuffle } from '../utils';
 import './style.css';
 
-const App = () => (
-  <div class="content">
-    <div class="wrapper">
-      <h1>Planet Wars</h1>
-    </div>
-    <Background />
-  </div>
-);
+function App() {
+  const [planetsId, setPlanetsIds] = useState([]);
+
+  useEffect(() => {
+    if (!planetsId.length) {
+      Planets.all()
+        .then(({ count }) => {
+          let ids = [];
+          for (let i = 1; i <= count; i++) ids.push(i);
+          setPlanetsIds(shuffle(ids));
+        })
+        .catch(e => {
+          console.error(e);
+          alert('Falha ao coletar informações sobre os planetas!');
+        });
+    }
+  }, [planetsId.length]);
+
+  return (
+    <>
+      <div className="content">
+        {planetsId.length ? <Game planetsId={planetsId} /> : <LoadingGame />}
+      </div>
+      <Background />
+    </>
+  );
+}
 
 export default App;
