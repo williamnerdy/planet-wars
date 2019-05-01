5e394326f1e79d645d23652cdea281619c5a64c8
Travis CI <leocardosoti@gmail.com>
2019-05-01 15:28:47 +0000

    Deploy to GitHub Pages

---
+++ precache-manifest.4ee66dc323507fc045efd7241dba93b8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913caf3b3e5002e557a9feae9003bc4c",
+    "url": "/planet-wars/index.html"
+  },
+  {
+    "revision": "86b05582b6882df12e5a",
+    "url": "/planet-wars/static/css/main.fdeabf04.chunk.css"
+  },
+  {
+    "revision": "eb28c269b131c7bb0255",
+    "url": "/planet-wars/static/js/2.b872d004.chunk.js"
+  },
+  {
+    "revision": "86b05582b6882df12e5a",
+    "url": "/planet-wars/static/js/main.51d12354.chunk.js"
+  },
+  {
+    "revision": "b968ed437db3db523900",
+    "url": "/planet-wars/static/js/runtime~main.d7f6cf48.js"
+  },
+  {
+    "revision": "35f31c7aa7cc82eaec5d5c37ef2de044",
+    "url": "/planet-wars/static/media/STARWARS.35f31c7a.woff"
+  },
+  {
+    "revision": "3f085be9c96d464c8ed2774e89793393",
+    "url": "/planet-wars/static/media/STARWARS.3f085be9.ttf"
+  },
+  {
+    "revision": "95e43dd634fa7abc1f0bac8278faf5e4",
+    "url": "/planet-wars/static/media/STARWARS.95e43dd6.svg"
+  },
+  {
+    "revision": "fa7a31d20cbd5f199216f667dfe9c5d8",
+    "url": "/planet-wars/static/media/STARWARS.fa7a31d2.eot"
+  },
+  {
+    "revision": "79e8fa64a8503259d3ff45d06fbc01b8",
+    "url": "/planet-wars/static/media/clouds.79e8fa64.png"
+  },
+  {
+    "revision": "d68ba016af5b23b7e35a19264c2c4540",
+    "url": "/planet-wars/static/media/imperial-march.d68ba016.mp3"
+  },
+  {
+    "revision": "6494ab5b3760b5e2cc46f5566a245371",
+    "url": "/planet-wars/static/media/stars.6494ab5b.png"
+  }
+]);
