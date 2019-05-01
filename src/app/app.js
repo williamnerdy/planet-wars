b1e3caeba1125266552def168f0267870a1de57f
William Cunha <william.cunha@wololo.tech>
2019-04-30 21:01:03 -0300

    Add app style

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import './app.css';
+import Background from '../components/background';
+import './style.css';
 
-function App() {
-  return <h3>Planet Wars</h3>;
-}
+const App = () => (
+  <div class="content">
+    <div class="wrapper">
+      <h1>Planet Wars</h1>
+    </div>
+    <Background />
+  </div>
+);
 
 export default App;
